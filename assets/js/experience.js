af34326e9dcbda87c311db9ab379ddef784d2d67
esmanda3w <esmanda3w@gmail.com>
2021-06-08 00:01:22 +0800

    Add experience

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,23 +6,9 @@ const exp = [
   {
     title: "Software Development Intern",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
-  },
-  {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-  },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    place: "DSTA",
+    time: "(May, 2021 - present)",
+    desp: "<li>Apply Design Innovation approaches to conduct user interviews and gather requirements.</li> <li>Design and develop prototype.</li> <li>Work directly with the customers to validate and refine their work.</li>",
   },
 ];
 
@@ -62,40 +48,10 @@ document.addEventListener("DOMContentLoaded", showCards2);
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title: "GirlScript Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/1.jpg",
+    title: "Project Kickstart 2020",
+    cardImage: "assets/images/experience-page/Project.png",
     description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
-  },
-  {
-    title: "StudentCode-in 2020",
-    cardImage: "assets/images/experience-page/2.jpg",
-    description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-  },
-  {
-    title: "PClub Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/3.jpg",
-    description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-  },
-  {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
-    description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
-  },
-  {
-    title: "30 DaysofFlutter",
-    cardImage: "assets/images/experience-page/5.jpg",
-    description:
-      "Helping beginners from their first steps in Flutter to building fully functional cross-platform applications.",
-  },
-  {
-    title: "Garuda Hacks",
-    cardImage: "assets/images/experience-page/6.jpg",
-    description:
-      "MLH sponsored Indonesia's premier Global Virtual Hackathon on 14 - 16th August 2020.",
+      "Introduced coding to children from disadvantaged backgrounds.",
   },
 ];
 
@@ -122,45 +78,45 @@ document.addEventListener("DOMContentLoaded", showCards);
 // Mentorship Card
 
 
-const mentorshipcards = document.querySelector(".mentorship-cards");
-const mentor = [
-  {
-    title: "HakinCode",
-    image: "assets/images/experience-page/hakin.png",
-    time: "06/2020 - 08/2020",
-    desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
-  },
-  {
-    title: "Google Summer of Code",
-    image: "assets/images/experience-page/gsoc.png",
-    time: "03/2020 - 08/2020",
-    desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
-  },
-];
+// const mentorshipcards = document.querySelector(".mentorship-cards");
+// const mentor = [
+//   {
+//     title: "HakinCode",
+//     image: "assets/images/experience-page/hakin.png",
+//     time: "06/2020 - 08/2020",
+//     desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
+//   },
+//   {
+//     title: "Google Summer of Code",
+//     image: "assets/images/experience-page/gsoc.png",
+//     time: "03/2020 - 08/2020",
+//     desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
+//   },
+// ];
 
-const showCards3 = () => {
-  let output = "";
-  mentor.forEach(
-    ({ title, image, time, desp}) =>
-      (output += `        
-      <div class="column mentorshipCard"> 
-      <div class="card card2 mentorshipCardCover">
-        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
-        <div class="information">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class=""sub-title">${time}</p>
-        </div>
-        <div class="more-information">
-        <ul class="list-group list-group-flush p-0 right-aligned">
-          <div class="list-group-item card2 disclaimer">${desp}</div>
-        </ul>
-        </div>
-        </div>
-      </div>
-      </div>
-      `)
-  );
-  mentorshipcards.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards3);
+// const showCards3 = () => {
+//   let output = "";
+//   mentor.forEach(
+//     ({ title, image, time, desp}) =>
+//       (output += `        
+//       <div class="column mentorshipCard"> 
+//       <div class="card card2 mentorshipCardCover">
+//         <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
+//         <div class="information">
+//         <div class="card-body">
+//           <h5 class="card-title">${title}</h5>
+//           <p class=""sub-title">${time}</p>
+//         </div>
+//         <div class="more-information">
+//         <ul class="list-group list-group-flush p-0 right-aligned">
+//           <div class="list-group-item card2 disclaimer">${desp}</div>
+//         </ul>
+//         </div>
+//         </div>
+//       </div>
+//       </div>
+//       `)
+//   );
+//   mentorshipcards.innerHTML = output;
+// };
+// document.addEventListener("DOMContentLoaded", showCards3);
