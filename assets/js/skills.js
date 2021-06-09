f86da619e737bc2b50fc9269f930a38f58311b47
esmanda3w <esmanda3w@gmail.com>
2021-06-09 23:14:10 +0800

    Add skills

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -5,28 +5,19 @@ const projectcards = document.querySelector(".projectcards");
 // Array of object for projects
 const projects = [
   {
-    title: "Quiz App",
-    cardImage: "assets/images/project-page/quiz.jpg",
-    description: "A quiz app built using basic react.",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
+    cardImage: "assets/images/devicon/java.png",
   },
   {
-    title: "Chess Engine",
-    cardImage: "assets/images/project-page/chess.jpg",
-    description: "A chess engine for a popular game dev engine.",
-    tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
-    Previewlink: "",
-    Githublink: "",
+    cardImage: "assets/images/devicon/python.png",
   },
   {
-    title: "Flappy Bird",
-    cardImage: "assets/images/project-page/flappybird.png",
-    description: "Flappy bird game built using React.js",
-    tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
-    Previewlink: "",
-    Githublink: "",
+    cardImage: "assets/images/devicon/js.png",
+  },
+  {
+    cardImage: "assets/images/devicon/html.png",
+  },
+  {
+    cardImage: "assets/images/devicon/css.png",
   },
 ];
 
@@ -34,23 +25,10 @@ const projects = [
 const showCards = () => {
   let output = "";
   projects.forEach(
-    ({ title, cardImage, tags, Previewlink, Githublink }) => {
+    ({cardImage}) => {
       (output += `       
     <div class="column skill-card card">
-      <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-        <div class="header">
-        </div>
-        <div class="data">
-          <div class="content">
-          <div class="title-div">
-            <h1 class="title"><a href="#">${title}</a></h1>
-            </div>
-        <ul class="menu-content"><br>
-              <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
-              <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
-            </ul>
-          </div>
-        </div>
+      <div class="wrapper" style="background: url(${cardImage}) center / contain no-repeat;">
       </div>
     </div>`)
     }
